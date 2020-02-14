b8ad7d9716e6de320d8d1f86615135e83427b4d0
davidkline-ms <david.kline@microsoft.com>
2020-02-14 14:53:07 -0800

    initial scaffolding for ar foundation observers

---
+++ Assets/MixedRealityToolkit.Providers/UnityAR/BaseUnityARSpatialObserver.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UnityAR
+{
+    /// <summary>
+    /// Base class used by Unity AR Foundation based spatial observers.
+    /// </summary>
+    public class BaseUnityARSpatialObserver : BaseSpatialObserver
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="spatialAwarenessSystem">The service instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public BaseUnityARSpatialObserver(
+            IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(spatialAwarenessSystem, name, priority, profile)
+        { }
+
+        // todo
+
+        #region IMixedRealitySpatialAwarenessObserver
+
+        /// <inheritdoc />
+        public override void Resume()
+        {
+            base.Resume();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Suspend()
+        {
+            base.Suspend();
+
+            // todo
+        }
+
+        #endregion IMixedRealitySpatialAwarenessObserver
+
+        #region IMixedRealityDataProvider
+
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Enable()
+        {
+            base.Enable();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Disable()
+        {
+            base.Disable();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Update()
+        {
+            base.Update();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Reset()
+        {
+            base.Reset();
+
+            // todo
+        }
+
+        /// <inheritdoc />
+        public override void Destroy()
+        {
+            // todo
+
+            base.Destroy();
+        }
+
+        #endregion IMixedRealityDataProvider
+    }
+}
---
+++ Assets/MixedRealityToolkit.Providers/UnityAR/UnityARSpatialPlaneObserver.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UnityAR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [MixedRealityDataProvider(
+        typeof(IMixedRealitySpatialAwarenessSystem),
+        SupportedPlatforms.Android | SupportedPlatforms.IOS,
+        "Unity AR Spatial Planes Observer",
+        "Profiles/DefaultMixedRealitySpatialPlanesObserverProfile.asset",
+        "MixedRealityToolkit.SDK")]
+    [HelpURL("https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation/SpatialAwareness/SpatialAwarenessGettingStarted.html")]
+    public class UnityARSpatialPlaneObserver :
+        BaseSpatialObserver,    // todo: Create base plane observer?
+        IMixedRealityCapabilityCheck
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="spatialAwarenessSystem">The service instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public UnityARSpatialPlaneObserver(
+            IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(spatialAwarenessSystem, name, priority, profile)
+        {
+            ReadProfile();
+        }
+
+        private void ReadProfile()
+        {
+            if (ConfigurationProfile == null)
+            {
+                // todo: Debug.LogWarning("A profile was not specified for the Unity AR Spatial Planes observer.");
+                return;
+            }
+
+            // todo
+        }
+
+        // todo
+
+        #region IMixedRealityCapabilityCheck
+
+        /// <inheritdoc />
+        public bool CheckCapability(MixedRealityCapability capability)
+        {
+            bool isSupported = false;
+
+    #if ARFOUNDATION_PRESENT
+            isSupported = (capability == MixedRealityCapability.SpatialAwarenessPlane);
+    #endif // ARFOUNDATION_PRESENT
+
+            return isSupported;
+        }
+
+        #endregion IMixedRealityCapabilityCheck
+    }
+}
